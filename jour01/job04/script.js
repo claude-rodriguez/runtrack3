4d04be323c72ee76f2b85eaf8b41e0608dd86d1d
rodriguez <claude.rodriguez@laplateforme.io>
2022-03-14 11:20:30 +0100

    job04

---
+++ jour01/job04/script.js
@@ -0,0 +1,23 @@
+//permet d'afficher le résultat (box+page)
+
+var year = prompt("Année : ");
+
+var result = bisextile(year);
+
+alert(result);
+// permat d'indiquer les mois de février de 29 jours
+
+function bisextile(année)
+{
+    var jours = new Date(année,2,0).getDate();
+    if( jours == 29)
+    {
+        document.write(année + ' est bissextile');
+        return(true);
+    }
+    else
+    {
+        document.write(année + " n'est pas bissextile");
+        return(false);
+    }
+}
