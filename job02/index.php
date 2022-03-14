7d9044a2e5f3cdaf9a83ee4e8be0c05c020cf5f2
rodriguez <claude.rodriguez@laplateforme.io>
2022-03-14 10:43:57 +0100

    job02

---
+++ job02/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Runtrack 3 - Jour 1 - Job 2</title>
+    
+    <script type = "text/javascript" src='script.js'></script>
+    
+</head>
+<body>
+    
+</body>
+</html>
---
+++ job02/script.js
@@ -0,0 +1,1 @@
+alert('Hello Javascript !'); //Affiche ce message dans la box
