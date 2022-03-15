a7aa7f192540735825bbd6996ab9ba8f873bc940
rodriguez <claude.rodriguez@laplateforme.io>
2022-03-15 10:48:44 +0100

    jour2job1

---
+++ jour2/job1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">  <title>Runtrack 3 - Jour 1 - Job 2</title>
+        
+        <script type = "text/javascript" src='script.js'></script>   
+    </head>
+    
+    <body>
+        <article id="citation">
+            La vie a beaucoup plus d’imagination que nous
+        </article>
+        <button onclick="citation()">citation</button>
+    </body>
+</html>
