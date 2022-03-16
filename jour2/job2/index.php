da8883374a234bcb5b689cb1a8ca7fb857e53c7a
rodriguez <claude.rodriguez@laplateforme.io>
2022-03-16 17:00:27 +0100

    jour2job2

---
+++ jour2/job2/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">  <title>Runtrack 3 - Jour 1 - Job 2</title>
+        
+        <script type = "text/javascript" src='script.js'></script>   
+    </head>
+    
+    <body>
+        <button id="button" onclick="showhide()">Afficher</button>
+    </body>
+</html>
