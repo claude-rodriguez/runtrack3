8e7b47a0e598b04f041585b00f2a81c972b916e9
rodriguez <claude.rodriguez@laplateforme.io>
2022-03-21 08:28:54 +0100

    jour4job1

---
+++ jour4/job1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">  <title>Runtrack 4 - Jour 1 - Job 1</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    </head>
+    
+    <body>
+            <button id="button" onclick="click()">afficher</button> 
+        <script type = "text/javascript" src="script.js"></script>   
+    </body>
+</html>
